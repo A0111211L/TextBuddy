c79b87f93b797d49017cd54f845b910c2bed988e
Justin <EzWearDiapers@hotmail.com>
2014-09-16 14:06:55 +0800

    Junit testing for add function

--- src/cs2103/TextBuddyTest.java
+++ src/cs2103/TextBuddyTest.java
@@ -9,11 +9,22 @@ public class TextBuddyTest {
 	
 	@Test
 	public void testIsAdded() throws IOException {
-		
 		// normal text
 		TextBuddy.executeCommand("add chicken wings");
-		TextBuddy.executeCommand("exit");
 		assertTrue(TextBuddy.isAdded("chicken wings"));
+		
+		// invalid command
+		TextBuddy.executeCommand("addd KFC");
+		assertFalse(TextBuddy.isAdded("KFC"));
+		
+		// add whitespace
+		TextBuddy.executeCommand("add ");
+		assertTrue(TextBuddy.isAdded(""));
+		
+		// add text with word add inside
+		TextBuddy.executeCommand("add ADDadd add me");
+		assertTrue(TextBuddy.isAdded("ADDadd add me"));
+		
 	}
 
 }
